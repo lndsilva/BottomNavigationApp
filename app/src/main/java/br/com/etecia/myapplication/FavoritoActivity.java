2da892e455bab3f937e5510d82cd1145b6ab00b7
manager <lndsilva@hotmail.com>
2019-08-19 21:01:37 -0300

    Bottom Navigation

---
+++ app/src/main/java/br/com/etecia/myapplication/FavoritoActivity.java
@@ -0,0 +1,13 @@
+package br.com.etecia.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FavoritoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.favorito_layout);
+    }
+}
---
+++ app/src/main/java/br/com/etecia/myapplication/MainActivity.java
@@ -0,0 +1,31 @@
+package br.com.etecia.myapplication;
+
+import android.content.Intent;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = (BottomNavigationView)findViewById(R.id.btnNavigationView);
+
+
+
+    }
+
+
+
+
+
+
+
+}
